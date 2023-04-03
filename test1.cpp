90325dc2b079c7e8e7d2b7cd909a17bdf4d76c02
UIT.21521875 <21521875@gm.uit.edu.vn>
2023-04-03 13:19:37 +0700

    create file

---
+++ test1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std ;
+void ktra( int A[], int n ){
+int k=0;
+for (int i=0;i<n;i++)
+{
+if (A[i]!= A[n-i-1])
+{
+k = 1 ;
+break ;
+}
+
+}
+if(k==1 )
+cout<<"Khong doi xung";
+else cout<<"Doi xung";
+}
+int main ()
+{
+int n,k,A[1000];
+do{
+cout<<"nhap n " ;
+cin>>n;
+}
+while(n<=1);
+for (int i=0;i<n;i++)
+{
+cout<<"Nhap A["<<i<<"]";
+cin >>A[i];
+}
+ktra( A , n);
+return 0 ;
+}
